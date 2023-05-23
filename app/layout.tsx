792e39d557b434fd1192cc6207fca3c89781c033
Peyman <this.peyman@gmail.com>
2023-05-23 15:54:59 +0330

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import { Metadata } from "next";
+
+import "@styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <div>Page</div>
+    )
+}
+
+export default Home
